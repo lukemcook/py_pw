f09d9d9e5cbe6dc256e61f488d901eee67a34821
lukemcook <95878598+lukemcook@users.noreply.github.com>
2024-05-08 11:31:48 +0900

    Update py_pw.py

--- py_pw.py
+++ py_pw.py
@@ -2,13 +2,13 @@ import random
 import string
 import pyperclip
 
-# Define characters for password creation
+# define characters for password creation
 upper_case = list(string.ascii_uppercase)
 lower_case = list(string.ascii_lowercase)
 other = "@*&#%^()-+="
 other = list(other)
 character_list = upper_case + lower_case + other
-# Ask for password length and assign characters randomly
+# ask for password length and assign characters randomly
 random.shuffle(character_list)
 pw = []
 pw_length = int(input("Enter desired password length, e.g. 12 for 12 characters: "))

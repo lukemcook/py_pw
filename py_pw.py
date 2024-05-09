7f7190cfa8656b2dc280a3bd11855c1bb025c232
lukemcook <95878598+lukemcook@users.noreply.github.com>
2024-05-09 14:42:48 +0900

    Update py_pw.py
    Added error handling, failed attempt tracking

--- py_pw.py
+++ py_pw.py
@@ -9,14 +9,29 @@ other = "@*&#%^()-+="
 other = list(other)
 digits = list(string.digits)
 character_list = upper_case + lower_case + other + digits
-# ask for password length and assign characters randomly
+# shuffle character list, create empty pw list
 random.shuffle(character_list)
 pw = []
-pw_length = int(input("Enter desired password length, e.g. 12 for 12 characters: "))
-# loop to adhere to desired password length
-for char in range(pw_length):
-    pw.append(random.choice(character_list))
-print(f"Your generated password is: {''.join(pw)}")
+# add variable for tracking attempts
+attempts = 0
+# loop and try/except for error handling
+while True:
+    try:
+        pw_length = int(input("Enter desired password length, e.g. 12 for 12 characters and be at least 5: "))
+        if pw_length <= 4:
+            raise ValueError
+# loop to match desired password length
+        for char in range(pw_length):
+            pw.append(random.choice(character_list))
+        print(f"Your generated password is: {''.join(pw)}")
 # pyperclip copies password to clipboard
-pyperclip.copy(''.join(pw))
-print(f"Copied {''.join(pw)} to clipboard")
+        pyperclip.copy(''.join(pw))
+        print(f"Copied {''.join(pw)} to clipboard")
+        break
+# except catches input error(s) and updates failed attempts
+    except ValueError:
+        attempts += 1
+        if attempts >= 3:
+            print("Exiting due to 3 incorrect attempts.")
+            break
+        print("Password should contain only whole numbers and contain 5 or more characters.")
